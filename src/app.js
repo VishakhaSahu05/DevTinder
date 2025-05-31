4b349754c1ef52c60f1dd599a14cdc4ca00a5d2e
VishakhaSahu05 <sahuvishakha756@gmail.com>
2025-05-31 21:21:11 +0530

    playing with routers

--- src/app.js
+++ src/app.js
@@ -2,21 +2,16 @@ const express = require("express");
 
 const app = express();
 
-app.get("/user" ,(req , res)=>{
-  res.send("server is working");
-});
-app.post("/user" , (req,res)=>{
-  res.send("data is posted successfully!");
-});
-app.delete("/user" ,(req , res)=>{
-    res.send("deleted successfully!");
-});
-app.use("/user" ,(req ,res)=>{
-    res.send("hello , hello , hello");
+app.
+get("/ab?c",(req , res)=>{
+  //console.log(req.params);
+  res.send("abc");
 });
 
 
-app.listen(3000 ,()=>{
+
+
+app.listen(5000 ,()=>{
   console.log("Server called successfully");
 });
 
