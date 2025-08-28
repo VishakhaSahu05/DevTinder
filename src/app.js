2a344032a698adc6326c880da455ed9d81d3b2cc
VishakhaSahu05 <sahuvishakha756@gmail.com>
2025-08-28 12:47:53 +0530

    fix

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+    origin: ["http://localhost:5173", "http://127.0.0.1:5173" , "https://your-production-domain.com"],
     credentials: true,
   },
 });
